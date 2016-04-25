2dd056812e7aaafe32ec9530e0c7053a1b44b32c
paras <parashbhujel@gmail.com>
2016-04-25 14:07:23 +0545

    UDPServer added

---
+++ UDPServer.java
@@ -0,0 +1,14 @@
+import java.io.*; 
+import java.util.*;
+public class UDPServer {
+ 
+    public UDPServer() {
+ 
+    }
+ 
+    public static void main(String[] args) {
+ 
+        FileRcv dirRcr = new FileRcv();
+ 
+    }
+}
